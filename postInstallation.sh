55f9827b32350f91a8c49a9ba82289dc9e7d8c9c
A-dams <40596554+A-dams@users.noreply.github.com>
2018-06-29 07:58:49 +0200

    Update postInstallation.sh

--- postInstallation.sh
+++ postInstallation.sh
@@ -5,11 +5,11 @@ if [ $UID -ne 0 ]; then
   exit 1
 fi
 #####
-# Liste des applications ànstaller: A adapter a vos besoins
+# list of applications we want to install (Adapt with your needs)
 liste="vim sudo ntpdate curl"
 #####
 
-# Mise a jour de la liste des depots
+
 # Update 
 echo -e "\n Mise a jour de la liste des depots\n"
 apt update
@@ -24,5 +24,33 @@ apt -y install $liste
 
 echo -e "\n FIN."
 
-#repo sources pour apt
+#Sources repository for apt
 wget -P /etc/apt/ https://repository.neuronesit.fr/repository/debian/
+
+#Firewall desactivation
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+#http + https + ssh
+echo "-A INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT" >> /etc/iptables/rules.v4
+echo "-A INPUT -p tcp -m tcp --dport 22 -j ACCEPT" >> /etc/iptables/rules.v4
+
+
+
+
+
+
+
+
+
+
+
+
+
